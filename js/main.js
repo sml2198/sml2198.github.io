b52dc69cb4abed32db2245483758b28d85a9ec92
William Falk-Wallace <wfalkwallace@gmail.com>
2013-12-06 15:49:33 -0500

    about page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+$(function () {
 
+	function changeMenu(dest) {
+		// $("#home, #about").css("display","none");
+		// $("#"+dest).css("display","block");
+		$("#nav-home").removeClass("active");
+		$("#nav-about").removeClass("active");
+		$("#home").addClass("hidden");
+		$("#about").addClass("hidden");
+		
+		$("#"+dest).removeClass("hidden");
+		$("#nav-"+dest).addClass("active");
+	}
+
+	changeMenu("home");
+	
+	$("#nav-home").click(function() {
+		changeMenu('home');
+	});
+	
+	$("#nav-about").click(function() {
+		changeMenu('about');
+	});
+
+});
