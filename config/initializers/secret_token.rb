27667c31fc0d4306e97cdcc36adaf67e726b8ba1
William Falk-Wallace <wgf2104@columbia.edu>
2014-05-03 16:01:56 -0400

    adds mongo rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sml2198GithubIo::Application.config.secret_key_base = '95b6f1d2e688eb57435f7701d54be332d41a4dd9610f1bd97632666a0bd1bd041db288ba4104bd1edd72c637e30b2f7f40213643a348ee72db445dce6045317f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sml2198GithubIo::Application.config.session_store :cookie_store, key: '_sml2198_github_io_session'
