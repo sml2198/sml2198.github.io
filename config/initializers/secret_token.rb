a37e5b774a9a8de651166305a5a2c85e79ed121c
William Falk-Wallace <wgf2104@columbia.edu>
2014-05-03 15:12:42 -0400

    adds rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sml2198GithubIo::Application.config.secret_key_base = 'e0c6f0fccbcddff34b77b161831ffd266859f9b681c62f595d1916bd0d70f50ae4693898c79cd1521b9aed5f97452a1416aaef910433ed92780d63ef44f38baf'
